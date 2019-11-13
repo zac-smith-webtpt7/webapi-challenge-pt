2b27e5fcd63defad6028b7b2f8ddb03b3fd12b18
Zac Smith <zrs3141592@gmail.com>
2019-11-12 20:27:21 -0700

    actionRouter complete and working

--- data/helpers/actionsRouter.js
+++ data/helpers/actionsRouter.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        message: `The action could not be retrieved`
+        message: `The action could not be retrieved. Error ${err}`
       });
     });
 });
@@ -28,15 +28,57 @@ router.get("/:id", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        message: `The action with id ${id} cannot be retrieved`
+        message: `The action with id ${id} cannot be retrieved. Error ${err}`
       });
     });
 });
 
 // POST
+router.post("/", (req, res) => {
+  const action = req.body;
+  actionDb
+    .insert(req.body)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `New action could not be added. Error ${err}`
+      });
+    });
+});
 
 // PUT /:id
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const update = req.body;
+
+  actionDb
+    .update(id, update)
+    .then(action => {
+      res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Action with id ${id} cannot be udated. Error ${err}`
+      });
+    });
+});
 
 // DELETE /:id
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  actionDb
+    .remove(id)
+    .then(action => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Action with id ${id} cannot be deleted. Error ${err}`
+      });
+    });
+});
 
 module.exports = router;
